1fe4e9cac986f1d52ec3ec9307a4ecd0a1776d60
BuildTools <unconfigured@null.spigotmc.org>
2024-10-01 00:56:14 +1000

    everything

--- chest_openable.gd
+++ chest_openable.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
 
+@export var Card_Display: Control
+
 func _ready():
 	monitoring = true
+	
 	
 
 
@@ -10,7 +13,7 @@ func _ready():
 func _on_body_entered(body): 
 	if body.name == "PlayerBody": 
 		$chest_animation2.play("chest_1")
-
+		Card_Display.visible = true
 
 func _on_body_exited(body: Node2D) -> void:
 		if body.name == "PlayerBody": 
