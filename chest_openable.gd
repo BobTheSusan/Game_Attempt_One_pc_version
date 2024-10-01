573b3f6f74e867c2bdfaab7d49767f7372f97001
BuildTools <unconfigured@null.spigotmc.org>
2024-10-01 22:26:57 +1000

    things appear and dissapear after going to chest

--- chest_openable.gd
+++ chest_openable.gd
@@ -13,10 +13,13 @@ func _ready():
 func _on_body_entered(body): 
 	if body.name == "PlayerBody": 
 		$chest_animation2.play("chest_1")
+		Card_Display.visible = true 
+		
 		
 
 func _on_body_exited(body: Node2D) -> void:
 		if body.name == "PlayerBody": 
 			$chest_animation2.play_backwards("chest_1")
+			Card_Display.visible = false
 			
 			
