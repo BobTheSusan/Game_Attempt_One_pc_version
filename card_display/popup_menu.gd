9aac3d00797ce361f9d005bf35a1109735197805
BuildTools <unconfigured@null.spigotmc.org>
2024-10-01 15:57:53 +1000

    committing

--- card_display/popup_menu.gd
+++ card_display/popup_menu.gd
@@ -1,20 +1,11 @@
-extends Control
+extends PopupMenu
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	visible = true 
-	var displayed: bool = false
-	
-
-func _display():
-	visible = true 
-	var displayed: bool = true
+	pass # Replace with function body.
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-
-
-			
