12fefa01779a1b7e482773bb4858291addf7bc57
BuildTools <unconfigured@null.spigotmc.org>
2024-10-01 12:55:12 +1000

    working state 1

--- chest_openable.gd
+++ chest_openable.gd
@@ -13,7 +13,7 @@ func _ready():
 func _on_body_entered(body): 
 	if body.name == "PlayerBody": 
 		$chest_animation2.play("chest_1")
-		Card_Display.visible = true
+		
 
 func _on_body_exited(body: Node2D) -> void:
 		if body.name == "PlayerBody": 
